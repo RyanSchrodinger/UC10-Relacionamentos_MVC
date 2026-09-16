fe881c379d0cd95250d1002ce69266c9b6dced70
Ryan <ronaldsagan000@gmail.com>
2026-09-16 10:41:44 -0300

    feat(ex08): add nova modelagem

--- Uc_10_Ryan_Relacionamentos_Descricao_00501/Uc_10_Ryan_Relacionamentos_Descricao_00501/Models/Automovel.cs
+++ Uc_10_Ryan_Relacionamentos_Descricao_00501/Uc_10_Ryan_Relacionamentos_Descricao_00501/Models/Automovel.cs
@@ -7,8 +7,14 @@ namespace Uc_10_Ryan_Relacionamentos_Descricao_00501.Models
         public int AutomovelId { get; set; }
         [Required(ErrorMessage = "O Modelo é obrigatório.")]
         public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "A Cor é obrigatória.")]
         public string Cor { get; set; }
+
+        [Required(ErrorMessage = "O Ano é obrigatório.")]
         public string Ano { get; set; }
+
+        [Required(ErrorMessage = "A Placa é obrigatória.")]
         public string Placa { get; set; }
 
         public int MarcaId { get; set; }
--- Uc_10_Ryan_Relacionamentos_Descricao_00501/Uc_10_Ryan_Relacionamentos_Descricao_00501/Models/Cliente.cs
+++ Uc_10_Ryan_Relacionamentos_Descricao_00501/Uc_10_Ryan_Relacionamentos_Descricao_00501/Models/Cliente.cs
@@ -8,7 +8,9 @@ namespace Uc_10_Ryan_Relacionamentos_Descricao_00501.Models
 
         [Required(ErrorMessage = "O Nome é obrigatório.")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O Email é obrigatório.")]
         public string Email { get;set; }
+        [Required(ErrorMessage = "O Cpf é obrigatório.")]
         public string Cpf { get; set; }
     }
 }

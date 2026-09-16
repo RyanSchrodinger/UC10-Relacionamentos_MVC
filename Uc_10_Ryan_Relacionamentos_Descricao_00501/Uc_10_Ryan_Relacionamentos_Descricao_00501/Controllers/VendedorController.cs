9a2a462251ff340b8aa30686261c0e43f816e6eb
RyanSchrodinger <ronaldsagan000@gmail.com>
2026-09-16 14:24:32 -0300

    feat(ex08): add paginas referente entidade vendedor

--- Uc_10_Ryan_Relacionamentos_Descricao_00501/Uc_10_Ryan_Relacionamentos_Descricao_00501/Controllers/VendedorController.cs
+++ Uc_10_Ryan_Relacionamentos_Descricao_00501/Uc_10_Ryan_Relacionamentos_Descricao_00501/Controllers/VendedorController.cs
@@ -24,6 +24,15 @@ namespace Uc_10_Ryan_Relacionamentos_Descricao_00501.Controllers
             return View(await _context.Vendedor.ToListAsync());
         }
 
+        public IActionResult NossaEquipe()
+        {
+            return View();
+        }
+
+        public IActionResult Atendimento()
+        {
+            return View();
+        }
         // GET: Vendedor/Details/5
         public async Task<IActionResult> Details(int? id)
         {
---
+++ Uc_10_Ryan_Relacionamentos_Descricao_00501/Uc_10_Ryan_Relacionamentos_Descricao_00501/Migrations/20260916171738_migraitonIniciaol.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Uc_10_Ryan_Relacionamentos_Descricao_00501.Migrations
+{
+    /// <inheritdoc />
+    public partial class migraitonIniciaol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.ClienteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Marca",
+                columns: table => new
+                {
+                    MarcaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marca", x => x.MarcaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendedor",
+                columns: table => new
+                {
+                    VendedorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendedor", x => x.VendedorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Automovel",
+                columns: table => new
+                {
+                    AutomovelId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ano = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Placa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MarcaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Automovel", x => x.AutomovelId);
+                    table.ForeignKey(
+                        name: "FK_Automovel_Marca_MarcaId",
+                        column: x => x.MarcaId,
+                        principalTable: "Marca",
+                        principalColumn: "MarcaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Venda",
+                columns: table => new
+                {
+                    VendaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClienteId = table.Column<int>(type: "int", nullable: false),
+                    VendedorId = table.Column<int>(type: "int", nullable: false),
+                    AutomovelId = table.Column<int>(type: "int", nullable: false),
+                    DataVenda = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ValorVenda = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Venda", x => x.VendaId);
+                    table.ForeignKey(
+                        name: "FK_Venda_Automovel_AutomovelId",
+                        column: x => x.AutomovelId,
+                        principalTable: "Automovel",
+                        principalColumn: "AutomovelId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Venda_Cliente_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Cliente",
+                        principalColumn: "ClienteId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Venda_Vendedor_VendedorId",
+                        column: x => x.VendedorId,
+                        principalTable: "Vendedor",
+                        principalColumn: "VendedorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Automovel_MarcaId",
+                table: "Automovel",
+                column: "MarcaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Venda_AutomovelId",
+                table: "Venda",
+                column: "AutomovelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Venda_ClienteId",
+                table: "Venda",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Venda_VendedorId",
+                table: "Venda",
+                column: "VendedorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Venda");
+
+            migrationBuilder.DropTable(
+                name: "Automovel");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "Vendedor");
+
+            migrationBuilder.DropTable(
+                name: "Marca");
+        }
+    }
+}

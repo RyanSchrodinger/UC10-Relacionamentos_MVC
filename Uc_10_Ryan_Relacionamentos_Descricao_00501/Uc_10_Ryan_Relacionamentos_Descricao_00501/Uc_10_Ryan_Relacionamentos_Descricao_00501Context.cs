751ff188f767bdd497c45a2a57130dfcd5debd60
RyanSchrodinger <ronaldsagan000@gmail.com>
2026-09-16 14:16:48 -0300

    feat(ex08): add controller de todas as classes

--- Uc_10_Ryan_Relacionamentos_Descricao_00501/Uc_10_Ryan_Relacionamentos_Descricao_00501/Uc_10_Ryan_Relacionamentos_Descricao_00501Context.cs
+++ Uc_10_Ryan_Relacionamentos_Descricao_00501/Uc_10_Ryan_Relacionamentos_Descricao_00501/Uc_10_Ryan_Relacionamentos_Descricao_00501Context.cs
@@ -1,6 +1,15 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Uc_10_Ryan_Relacionamentos_Descricao_00501.Models;
 
 public class Uc_10_Ryan_Relacionamentos_Descricao_00501Context(DbContextOptions<Uc_10_Ryan_Relacionamentos_Descricao_00501Context> options) : DbContext(options)
 {
     public DbSet<Uc_10_Ryan_Relacionamentos_Descricao_00501.Models.Automovel> Automovel { get; set; } = default!;
+
+public DbSet<Uc_10_Ryan_Relacionamentos_Descricao_00501.Models.Cliente> Cliente { get; set; } = default!;
+
+public DbSet<Uc_10_Ryan_Relacionamentos_Descricao_00501.Models.Marca> Marca { get; set; } = default!;
+
+public DbSet<Uc_10_Ryan_Relacionamentos_Descricao_00501.Models.Venda> Venda { get; set; } = default!;
+
+public DbSet<Uc_10_Ryan_Relacionamentos_Descricao_00501.Models.Vendedor> Vendedor { get; set; } = default!;
 }
